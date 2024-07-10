42de0b05bf5718d31265d6adc32006211bce17cd
federico722 <126474648+federico722@users.noreply.github.com>
2024-07-10 09:47:41 -0500

    Update to index.html

--- routes/register.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import registerController from '../controllers/register-controller';
-const router = express.Router();
-
-
-router.post('/', registerController);
-
-
-export default router;
